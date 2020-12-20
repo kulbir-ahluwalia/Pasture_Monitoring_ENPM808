56b68e8265368e4713ca8f0853d4e465880012b3
kulbir-ahluwalia <kulbir@terpmail.umd.edu>
2020-12-20 18:58:23 -0500

    added FINAL scripts

---
+++ FINAL_SCRIPTS/reading_patches_data.py
@@ -0,0 +1,111 @@
+#import bpy         #bpy = blender python
+import math
+#import pandas as pd
+#import xlrd
+#from time import process_time
+#import csv
+import pickle
+
+import time
+#import multiprocessing 
+from multiprocessing import Pool
+import pandas as pd
+import numpy as np
+
+
+with open('/home/ksa/Desktop/Pasture_Monitoring/FINAL_SCRIPTS/patch_generation/patches/npy_patches/npypatch_1_1_2by2_pasturesize10by10mnpy_array.npy', 'rb') as f:
+    x_y_day_heights_coords = np.load(f)
+
+print(x_y_day_heights_coords)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
